9628bcf6ea2ccd1f98364461e337bd6e862f4d06
Cleiton <cleitonwoycik@outlook.com>
2021-06-22 20:16:08 -0300

    creating user

---
+++ README.md
@@ -0,0 +1,21 @@
+# NlW valoriza
+
+##regras 
+
+- Cadastro de usuários
+
+    [] Não é permitido cadastrar mais de um usuário com o mesmo email
+
+    [] Não é permitido cadastrar usuário sem email
+
+- Cadastro de tag
+
+    [] Não é permetido cadastrar mais de uma tag com o mesmo nome
+
+    [] Não é permetido cadastrar tag sem nome
+
+    [] Não é permetido cadastro por usuários que não sejam administradores
+
+#
+
+# 
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+
+interface ICreateUser {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+    async execute({ name, email, admin }: ICreateUser) {
+        const usersRepositories = getCustomRepository(UsersRepositories);
+
+        if (!email) {
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await usersRepositories.findOne({ email });
+
+        if (userAlreadyExists) {
+            throw new Error("User Already Exists!");
+        }
+
+        const user = usersRepositories.create({
+            name,
+            email,
+            admin,
+        });
+
+        await usersRepositories.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService };
