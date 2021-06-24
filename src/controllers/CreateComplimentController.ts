4451ee02c9ba4d97c5f5277fcccdb4a2a4f5916b
Cleiton <cleitonwoycik@outlook.com>
2021-06-24 17:06:09 -0300

    creating first compliment

---
+++ src/controllers/CreateComplimentController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { ComplimentService } from "../services/CreateComplimentService";
+
+
+
+class CreateComplimentController {
+    async handle(req: Request, res: Response) {
+        const { tag_id, user_receiver, user_sender, message } = req.body;
+
+        const complimentService = new ComplimentService();
+
+        const compliment = await complimentService.execute({
+            tag_id,
+            user_receiver,
+            user_sender,
+            message
+        });
+
+        res.status(200).json({ compliment });
+    }
+}
+
+export { CreateComplimentController };
