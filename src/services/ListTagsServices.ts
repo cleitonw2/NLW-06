7ee0b18e890610021710882783251a09bd4d450b
Cleiton <cleitonwoycik@outlook.com>
2021-06-25 22:30:26 -0300

    adding class-transformer

--- src/services/ListTagsServices.ts
+++ src/services/ListTagsServices.ts
@@ -1,3 +1,4 @@
+import { classToPlain } from "class-transformer";
 import { getCustomRepository } from "typeorm";
 import { TagsRepositories } from "../repositories/TagsRepositories";
 
@@ -8,7 +9,7 @@ class ListTagsServices {
 
         const tags = await tagsRepositories.find();
 
-        return tags;
+        return classToPlain(tags);
     }
 }
 
